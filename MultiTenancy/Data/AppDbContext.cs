cb49cc8a7afa4ee7638846fa0f4f6e12f1ba4620
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-03 23:09:38 -1200

    Add AppDbContext

---
+++ MultiTenancy/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenancy.Models;
+
+namespace MultiTenancy.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        private readonly ITenantService _tenantService;
+
+        public string TenantId { get; set; }
+       
+        public AppDbContext(DbContextOptions<AppDbContext> options, ITenantService tenantService) : base(options) 
+        {
+            _tenantService = tenantService;
+            TenantId = _tenantService.GetCurrentTenant().TId;
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasQueryFilter(e => e.TenantId == TenantId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var tenantConnectionString = _tenantService.GetConnectionString();
+            if(!string.IsNullOrEmpty(tenantConnectionString))
+            {
+                var dbProvider = _tenantService.GetDatabaseProvider();
+
+                if(dbProvider?.ToLower() == "mssql")
+                {
+                    optionsBuilder.UseSqlServer(tenantConnectionString);
+                }
+            }
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<IMustHaveTenant>().Where(e => e.State == EntityState.Added))
+            {
+                entry.Entity.TenantId = TenantId;
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
