86af4d2f55e7a687eaa5b2f929cc3727d73be97a
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-03 23:28:40 -1200

    rejester AppDbContext in connectionString at Program File

--- MultiTenancy/Program.cs
+++ MultiTenancy/Program.cs
@@ -1,11 +1,35 @@
 
 
+using Microsoft.EntityFrameworkCore;
+using MultiTenancy.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<TenantSettings>(builder.Configuration.GetSection(nameof(TenantSettings)));
 
 // Add services to the container.
 TenantSettings options = new();
 builder.Configuration.GetSection(nameof(TenantSettings)).Bind(options);
+
+var defaultDbProvider = options.Defaults.DBProvider;
+if(defaultDbProvider.ToLower() == "mssql")
+{
+    builder.Services.AddDbContext<AppDbContext>(m => m.UseSqlServer());
+}
+
+foreach (var tenant in options.Tenants)
+{
+    var connectionString = tenant.ConnectionString ?? options.Defaults.ConnectionString;
+
+    using var scope = builder.Services.BuildServiceProvider().CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    dbContext.Database.SetConnectionString(connectionString);
+
+    if (dbContext.Database.GetPendingMigrations().Any())
+    {
+        dbContext.Database.Migrate();
+    }
+}
 builder.Services.AddScoped<ITenantService, TenantService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddControllers();
