7df63498bd4270f91728f853170094813713f551
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-09-04 00:54:16 -1200

    Add Service repository for product

---
+++ MultiTenancy/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenancy.Data;
+using MultiTenancy.Models;
+
+namespace MultiTenancy.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductService(AppDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<Product> CreatedAsync(Product product)
+        {
+            _dbContext.Products.Add(product);
+            await _dbContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetAllAsync()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Products.FindAsync(id);
+        }
+    }
+}
